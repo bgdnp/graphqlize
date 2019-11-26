3355ff76c7c89f5f80d30f0d74bd4a009b5c09df
Bogdan Petkovic <b.petkovic@levi9.com>
2019-11-26 09:51:37 +0100

    Field type handling

--- src/decorators/field.decorator.ts
+++ src/decorators/field.decorator.ts
@@ -1,11 +1,63 @@
-export function Field(): PropertyDecorator {
+import 'reflect-metadata'
+
+type FieldOptions = {
+  nullable: boolean
+  nullableList?: boolean
+  type?: Function | [Function]
+}
+
+// export function Field(): PropertyDecorator
+export function Field(typeOrOptions?: Function | [Function] | FieldOptions): PropertyDecorator {
+  let type: string
+  let list: boolean
+  let nullable: boolean
+  let nullableList: boolean
+
   return (target: any, propertyKey: string) => {
+    if (!typeOrOptions) {
+      type = Reflect.getMetadata('design:type', target, propertyKey).name
+      list = false
+    }
+
+    if (typeof typeOrOptions === 'function') {
+      type = typeOrOptions.name
+      list = false
+    }
+
+    if (Array.isArray(typeOrOptions)) {
+      type = typeOrOptions[0].name
+      list = true
+    }
+
+    if (typeof typeOrOptions === 'object' && !Array.isArray(typeOrOptions)) {
+      if (!typeOrOptions.type) {
+        type = Reflect.getMetadata('design:type', target, propertyKey).name
+        list = false
+      }
+
+      if (typeof typeOrOptions.type === 'function') {
+        type = typeOrOptions.type.name
+        list = false
+      }
+
+      if (Array.isArray(typeOrOptions.type)) {
+        type = typeOrOptions.type[0].name
+        list = true
+      }
+
+      nullable = typeOrOptions.nullable
+      nullableList = typeOrOptions.nullableList
+    }
+
     if (!target.__fieldsMap) {
       target.__fieldsMap = {}
     }
 
     target.__fieldsMap[propertyKey] = {
-      type: 'String',
+      type,
+      list,
+      nullable,
+      nullableList,
     }
   }
 }
--- src/utilities/generator.ts
+++ src/utilities/generator.ts
@@ -1,13 +1,23 @@
-import { GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql'
+import { GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLBoolean, GraphQLNonNull, GraphQLList } from 'graphql'
 import { storage } from '../metadata/storage'
 
 export class Generator {
+  private defaults
+
   private entities
 
   private types: { [name: string]: GraphQLObjectType } = {}
 
   private scalars = {
     String: GraphQLString,
+    Boolean: GraphQLBoolean,
+  }
+
+  constructor(defaults?) {
+    this.defaults = defaults || {
+      nullable: false,
+      nullableList: false,
+    }
   }
 
   public createSchema() {
@@ -26,7 +36,7 @@ export class Generator {
     let fields = {}
 
     for (let key in this.entities.query.fields) {
-      fields[key] = { type: this.getType(this.entities.query.fields[key].type) }
+      fields[key] = { type: this.getType(this.entities.query.fields[key]) }
     }
 
     return new GraphQLObjectType({
@@ -35,19 +45,57 @@ export class Generator {
     })
   }
 
-  private getType(typeName) {
+  private getType(typeMeta) {
+    const typeName: string = typeMeta.type
+
+    if (typeof typeMeta.nullable === 'undefined') {
+      typeMeta.nullable = this.defaults.nullable
+    }
+
+    if (typeof typeMeta.nullableList === 'undefined') {
+      typeMeta.nullableList = this.defaults.nullableList
+    }
+
     if (this.scalars[typeName]) {
-      return this.scalars[typeName]
+      let t = this.scalars[typeName]
+
+      if (!typeMeta.nullable) {
+        t = GraphQLNonNull(t)
+      }
+
+      if (typeMeta.list) {
+        t = GraphQLList(t)
+
+        if (!typeMeta.nullableList) {
+          t = GraphQLNonNull(t)
+        }
+      }
+
+      return t
     }
 
     if (this.types[typeName]) {
-      return this.types[typeName]
+      let t: any = this.types[typeName]
+
+      if (!typeMeta.nullable) {
+        t = GraphQLNonNull(t)
+      }
+
+      if (typeMeta.list) {
+        t = GraphQLList(t)
+
+        if (!typeMeta.nullableList) {
+          t = GraphQLNonNull(t)
+        }
+      }
+
+      return t
     }
 
     let fields = {}
 
     for (let key in this.entities.types[typeName].fields) {
-      fields[key] = { type: this.getType(this.entities.types[typeName].fields[key].type) }
+      fields[key] = { type: this.getType(this.entities.types[typeName].fields[key]) }
     }
 
     this.types[typeName] = new GraphQLObjectType({
@@ -55,6 +103,20 @@ export class Generator {
       fields: () => fields,
     })
 
-    return this.types[typeName]
+    let t: any = this.types[typeName]
+
+    if (!typeMeta.nullable) {
+      t = GraphQLNonNull(t)
+    }
+
+    if (typeMeta.list) {
+      t = GraphQLList(t)
+
+      if (!typeMeta.nullableList) {
+        t = GraphQLNonNull(t)
+      }
+    }
+
+    return t
   }
 }
