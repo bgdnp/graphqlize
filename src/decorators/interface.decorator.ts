647bac02aa37a24ab68e8b2b65b3b724f55fa3c6
Bogdan Petkovic <b.petkovic@levi9.com>
2019-11-29 14:31:41 +0100

    Revert all

--- src/decorators/interface.decorator.ts
+++
@@ -1,7 +0,0 @@
-import { storage } from '../metadata/storage'
-
-export function Interface(): ClassDecorator {
-  return (constructorFn: any) => {
-    storage.createInterfaceDefinition(constructorFn)
-  }
-}
