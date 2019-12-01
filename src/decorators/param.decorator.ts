69335ce961a30f4cf7415e131c58b40b6fc56fe7
Bogdan Petkovic <bogdanpetkovic@outlook.com>
2019-12-01 21:31:03 +0100

    Refactoring

--- src/decorators/param.decorator.ts
+++ src/decorators/param.decorator.ts
@@ -2,26 +2,7 @@ import { PARAMETERS_METADATA, FUNCTION_PARAM_TYPES } from '../constants'
 import { TParameter, TParamOptions, TTypeOptions } from '../typings'
 
 export function Param(name: string, paramOptions?: TParamOptions): ParameterDecorator {
-  let type: string
-  let options: TTypeOptions
-
-  if (paramOptions) {
-    if (typeof paramOptions.type === 'function') {
-      type = paramOptions.type.name
-      options = {
-        isRequired: !(paramOptions.nullable || false),
-        isList: false,
-        isListRequired: false,
-      }
-    } else if (Array.isArray(paramOptions.type)) {
-      type = paramOptions.type[0].name
-      options = {
-        isRequired: !(paramOptions.nullable || false),
-        isList: true,
-        isListRequired: !(paramOptions.nullableList || false),
-      }
-    }
-  }
+  let { type, options } = processOptions(paramOptions)
 
   return (target: any, propertyKey: string, parameterIndex: number) => {
     type = type || Reflect.getOwnMetadata(FUNCTION_PARAM_TYPES, target, propertyKey)[parameterIndex].name
@@ -43,4 +24,29 @@ export function Param(name: string, paramOptions?: TParamOptions): ParameterDeco
 
     Reflect.defineMetadata(PARAMETERS_METADATA, metadata, target, propertyKey)
   }
+}
+
+function processOptions(paramOptions: TParamOptions): { type: string; options: TTypeOptions } {
+  let type: string
+  let options: TTypeOptions
+
+  if (paramOptions) {
+    if (typeof paramOptions.type === 'function') {
+      type = paramOptions.type.name
+      options = {
+        isRequired: !(paramOptions.nullable || false),
+        isList: false,
+        isListRequired: false,
+      }
+    } else if (Array.isArray(paramOptions.type)) {
+      type = paramOptions.type[0].name
+      options = {
+        isRequired: !(paramOptions.nullable || false),
+        isList: true,
+        isListRequired: !(paramOptions.nullableList || false),
+      }
+    }
+  }
+
+  return { type, options }
 }
--- src/decorators/query.decorator.ts
+++ src/decorators/query.decorator.ts
@@ -3,50 +3,11 @@ import { FUNCTION_RETURN_TYPE, PARAMETERS_METADATA } from '../constants'
 import { TParameter, TQueryOptions, TTypeOptions } from '../typings'
 
 export function Query(typeOrOptions?: Function | [Function] | TQueryOptions): MethodDecorator {
+  let { type, options } = processOptions(typeOrOptions)
+
   return (target: any, propertyKey: string) => {
-    let type: string
-    let options: TTypeOptions
-
-    if (typeof typeOrOptions === 'function') {
-      type = typeOrOptions.name
-      options = {
-        isRequired: true,
-        isList: false,
-        isListRequired: false,
-      }
-    } else if (Array.isArray(typeOrOptions)) {
-      type = typeOrOptions[0].name
-      options = {
-        isRequired: true,
-        isList: true,
-        isListRequired: true,
-      }
-    } else if (typeof typeOrOptions === 'object') {
-      if (typeof typeOrOptions.type === 'function') {
-        type = typeOrOptions.type.name
-        options = {
-          isRequired: !typeOrOptions.nullable,
-          isList: false,
-          isListRequired: !typeOrOptions.nullableList,
-        }
-      } else if (Array.isArray(typeOrOptions.type)) {
-        type = typeOrOptions.type[0].name
-        options = {
-          isRequired: !typeOrOptions.nullable,
-          isList: true,
-          isListRequired: !typeOrOptions.nullableList,
-        }
-      }
-    } else {
-      type = Reflect.getOwnMetadata(FUNCTION_RETURN_TYPE, target, propertyKey).name
-      options = {
-        isRequired: true,
-        isList: false,
-        isListRequired: false,
-      }
-    }
-
     const parameters: TParameter[] = Reflect.getOwnMetadata(PARAMETERS_METADATA, target, propertyKey) || []
+    type = type || Reflect.getOwnMetadata(FUNCTION_RETURN_TYPE, target, propertyKey).name
 
     storage.addQueryField({
       name: propertyKey,
@@ -56,4 +17,51 @@ export function Query(typeOrOptions?: Function | [Function] | TQueryOptions): Me
       options,
     })
   }
+}
+
+function processOptions(
+  typeOrOptions: Function | [Function] | TQueryOptions,
+): { type?: string; options: TTypeOptions } {
+  let type: string
+  let options: TTypeOptions
+
+  if (typeof typeOrOptions === 'function') {
+    type = typeOrOptions.name
+    options = {
+      isRequired: true,
+      isList: false,
+      isListRequired: false,
+    }
+  } else if (Array.isArray(typeOrOptions)) {
+    type = typeOrOptions[0].name
+    options = {
+      isRequired: true,
+      isList: true,
+      isListRequired: true,
+    }
+  } else if (typeof typeOrOptions === 'object') {
+    if (typeof typeOrOptions.type === 'function') {
+      type = typeOrOptions.type.name
+      options = {
+        isRequired: !typeOrOptions.nullable,
+        isList: false,
+        isListRequired: !typeOrOptions.nullableList,
+      }
+    } else if (Array.isArray(typeOrOptions.type)) {
+      type = typeOrOptions.type[0].name
+      options = {
+        isRequired: !typeOrOptions.nullable,
+        isList: true,
+        isListRequired: !typeOrOptions.nullableList,
+      }
+    }
+  } else {
+    options = {
+      isRequired: true,
+      isList: false,
+      isListRequired: false,
+    }
+  }
+
+  return { type, options }
 }
