335fe7deafa2c11668739fc6014a61075246d5d9
Bogdan Petkovic <b.petkovic@levi9.com>
2020-03-12 08:09:43 +0100

    subscription type set to required

--- src/decorators/subscription.decorator.ts
+++ src/decorators/subscription.decorator.ts
@@ -1,14 +1,13 @@
 import { storage } from '../utilities/storage'
 import { processQueryOptions } from '../helpers'
-import { PARAMETERS_METADATA, FUNCTION_RETURN_TYPE } from '../constants'
+import { PARAMETERS_METADATA } from '../constants'
 import { TCreateQueryOptions, TParameter } from '../typings'
 
-export function Subscription(typeOrOptions?: Function | [Function] | TCreateQueryOptions): MethodDecorator {
+export function Subscription(typeOrOptions: Function | [Function] | Required<TCreateQueryOptions>): MethodDecorator {
   let { type, options } = processQueryOptions(typeOrOptions)
 
   return (target: any, propertyKey: string) => {
     const parameters: TParameter[] = Reflect.getOwnMetadata(PARAMETERS_METADATA, target, propertyKey) || []
-    type = type || Reflect.getOwnMetadata(FUNCTION_RETURN_TYPE, target, propertyKey).name
 
     storage.addSubscriptionField({
       name: propertyKey,
